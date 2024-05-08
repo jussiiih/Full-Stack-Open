26e832a561df8b28bcf5577eed9b1c211830eed6
= <=>
2024-05-08 09:04:13 +0300

    tehtavat 2.19 ja 2.20

--- osa2/maiden_tiedot/src/components/NationList.jsx
+++ osa2/maiden_tiedot/src/components/NationList.jsx
@@ -1,6 +1,27 @@
 import NationData from './NationData'
+import { useState } from 'react'
+
 
 const NationList = ({nationsToShow}) => {
+    const [selectedNation, setSelectedNation] = useState(null)
+
+    const handleShowButton = (nation) => {
+        setSelectedNation(nation)
+    }
+
+    const handleBackButton = () => {
+        setSelectedNation(null)
+    }
+
+    if (selectedNation) {
+        return (
+            <div>
+                <button onClick={handleBackButton}>Back</button>
+                <NationData nation={selectedNation}/>
+            </div>
+        )
+    }
+
     if (nationsToShow.length > 10) {
         return <p>Too many matches, specify another filter.</p>
     }
@@ -8,7 +29,7 @@ const NationList = ({nationsToShow}) => {
     else if (nationsToShow.length === 1) {
         return (
             <div>
-                <NationData nation ={nationsToShow[0]}/>
+                <NationData nation={nationsToShow[0]}/>
             </div>
         )
 
@@ -19,6 +40,7 @@ const NationList = ({nationsToShow}) => {
                 {nationsToShow.map((nation, index) => (
                 <li key={index}>
                     {nation}
+                    <button onClick={() => handleShowButton(nation)}>Show</button>
                 </li>
                 ))}
             </ul>
