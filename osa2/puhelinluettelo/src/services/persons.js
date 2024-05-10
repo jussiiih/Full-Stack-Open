f8412506d7ed2f155f0c970c872bc9a0ee837f53
= <=>
2024-05-10 10:36:24 +0300

    Muutettu sopimaan tehtävän 3.9 kanssa

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3002/persons'
+//Toimii paikallisesti
+//const baseUrl = 'http://localhost:3002/persons'
+
+//Toimii backendin kanssa
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
