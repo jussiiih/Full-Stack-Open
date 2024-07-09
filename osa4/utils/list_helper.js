baf3ef16cbb1cafd20a163040429d28f56ecc053
= <=>
2024-07-09 15:41:33 +0300

    tehtava 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,31 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length !== 0) {
+        const totalLikes = blogs.reduce(function(sum, blog) {
+            return sum + blog.likes}, 0)
+        return totalLikes
+    }
+    else {
+        return 0
+    }
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length !== 0) {
+        const max_likes = Math.max(...(blogs.map((blog) => blog.likes)))
+        const favorite_blog = blogs.find((blog) => blog.likes === max_likes)
+        const { _id, url, __v, ...result } = favorite_blog
+        return result
+    }
+    else {
+        return 0
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
