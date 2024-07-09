87a61aa8434827c174d1e231731e56dbec5815f6
= <=>
2024-07-09 12:02:01 +0300

    tehtava 4.2. valmis

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -5,18 +5,18 @@ const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  blogSchema.set('toJSON', {
-      transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
-    })
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-    module.exports = mongoose.model('Blog', blogSchema)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
