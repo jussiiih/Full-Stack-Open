84074f7a7a06c96d4dad24f915f0f27905e081d6
= <=>
2024-07-09 11:30:46 +0300

    tehtävä 4.2. app.js

---
+++ osa4/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const Blog =  require('./models/blog')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
--- osa4/index.js
+++ osa4/index.js
@@ -1,18 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const Blog =  require('./models/blog')
-const logger = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-
-
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
