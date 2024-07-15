4829267def16c5c3b5a2217eb9f414224da6f2b0
= <=>
2024-07-15 10:07:18 +0300

    tehtävä 4.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,23 +1,36 @@
 const blogsRouter = require('express'). Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => next(error))
+    //const user = await User.findById(body.user_id)
+    const user = await User.findOne({})
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        //user: body.user_id
+        user: user._id
+    })
+    try {
+        const savedBlog = await blog.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.status(201).json(savedBlog)
+
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
