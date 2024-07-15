3dc0baa63d17eefe73034283d8d63dfa7c5f6d29
= <=>
2024-07-15 13:17:52 +0300

    tehtävä 4.20

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,4 +1,4 @@
-const blogsRouter = require('express'). Router()
+const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
@@ -8,18 +8,19 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
-const getTokenFrom = request => {
+/*const getTokenFrom = request => {
     const authorization = request.get('authorization')
     if (authorization && authorization.startsWith('Bearer ')) {
         return authorization.replace('Bearer ', '')
     }
     return null
-}
+}*/
 
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    //const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' })
     }
--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -1,5 +1,16 @@
 const logger = require('./logger')
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token = authorization.replace('Bearer ', '')
+    }
+    else {
+        request.token = null
+    }
+    next ()
+}
+
 const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
@@ -17,5 +28,6 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
