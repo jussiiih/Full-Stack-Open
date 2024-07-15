f3c9149df82661ec000b219e3e71a19da3f8fc4e
= <=>
2024-07-15 11:38:38 +0300

    tehtävä 4.19

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,23 +1,35 @@
 const blogsRouter = require('express'). Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    //const user = await User.findById(body.user_id)
-    const user = await User.findOne({})
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        //user: body.user_id
         user: user._id
     })
     try {
