b9399bbabd90e24b8fa90e3c9f31a8bd5edda123
= <=>
2024-07-16 12:15:59 +0300

    tehtävä 5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -18,6 +18,14 @@ const App = () => {
     }
   }, [user])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }  
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -26,6 +34,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user)
+      )
+      
       setUser(user)
       setUsername('')
       setPassword('')
@@ -61,10 +73,18 @@ const App = () => {
 
   )
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    setUser('')
+    window.localStorage.clear()
+  }
+
   const blogList = () => (
     <div>
       <h2>Blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in 
+        <button type='submit' onClick={handleLogout}>Logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog}/>
       )}
