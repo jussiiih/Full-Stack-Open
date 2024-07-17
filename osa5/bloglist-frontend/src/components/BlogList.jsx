257072aad26e819a65df7315fa687c090a82ce44
= <=>
2024-07-17 13:58:16 +0300

    tehtävä 5.10

--- osa5/bloglist-frontend/src/components/BlogList.jsx
+++ osa5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,11 +1,14 @@
 import Blog from './Blog'
 
-const blogList = ({ blogs, setBlogs, user }) => (
+const blogList = ({ blogs, setBlogs, user }) => {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)  
+  return (
     <div>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} setBlogs={setBlogs} user={user}/>
-      )}
-  </div>
+        {sortedBlogs.map(blog =>
+          <Blog key={blog.id} blog={blog} setBlogs={setBlogs} user={user}/>
+        )}
+    </div>
   )
+}
 
 export default blogList
