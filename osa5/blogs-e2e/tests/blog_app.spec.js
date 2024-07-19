f8c90e91b04e80337b521bc7288e3f1d27758cc2
= <=>
2024-07-19 10:02:23 +0300

    tehtävä 5.21

--- osa5/blogs-e2e/tests/blog_app.spec.js
+++ osa5/blogs-e2e/tests/blog_app.spec.js
@@ -55,14 +55,13 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'Login' }).click()
     })
   
-    test.only('a new blog can be created', async ({ page }) => {
+    test('a new blog can be created', async ({ page }) => {
       await page.getByRole('button', { name: 'New blog' }).click()
       await page.getByTestId('title').fill('An Awesome Blog Title')
       await page.getByTestId('author').fill('C. Author')
       await page.getByTestId('url').fill('www.awesomeblog.com')
       await page.getByRole('button', { name: 'Create' }).click()
 
-      await expect(page.getByTestId('bloglist').getByText('An Awesome Blog Title').nth(1)).toBeVisible()
       await expect(page.getByText('An Awesome Blog Title').nth(1)).toBeVisible()
 
 
@@ -84,18 +83,18 @@ describe('Blog app', () => {
         await expect(page.getByText('Likes: 1')).toBeVisible()
       })
 
-      /*test.only('Blog can be deleted', async ({ page }) => {
+      test.only('Blog can be deleted', async ({ page }) => {
         await page.getByRole('button', { name: 'View' }).click()
+        
+        page.on('dialog', dialog => dialog.accept())
         await page.getByRole('button', { name: 'Remove' }).click()
         
-    
-
         await expect(page.getByText('An Awesome Blog Title').nth(1)).not.toBeVisible()
         await expect(page.getByText('C. Author')).not.toBeVisible()
         await expect(page.getByText('www.awesomeblog.com')).not.toBeVisible()
 
 
-      })*/
+      })
 
     })
 
