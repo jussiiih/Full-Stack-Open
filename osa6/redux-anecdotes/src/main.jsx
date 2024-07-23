5ef3338695e0b64ce5ba9367876b7836f7c8fd2c
= <=>
2024-07-23 15:38:37 +0300

    tehtävä 6.14

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -2,6 +2,15 @@ import ReactDOM from 'react-dom/client'
 import store from './components/store'
 import { Provider } from 'react-redux'
 import App from './App'
+import anecdoteService from './services/anecdotes'
+import appendReducer, {appendAnecdote} from './reducers/anecdoteReducer'
+
+
+anecdoteService.getAll().then(anecdotes =>
+  anecdotes.forEach(anecdote => {
+    store.dispatch(appendAnecdote(anecdote))    
+  })
+)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
