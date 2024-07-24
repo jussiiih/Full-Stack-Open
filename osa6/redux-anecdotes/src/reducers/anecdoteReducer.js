01b526e2c6c6d8b7c6d5807d241bad3446700bbc
= <=>
2024-07-24 09:04:27 +0300

    tehtävä 6.16

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdote',
@@ -22,11 +23,23 @@ const anecdoteSlice = createSlice({
 
   appendAnecdote (state, action) {
     state.push(action.payload)
+  },
+
+  setAnecdotes(state, action) {
+    return action.payload
   }
 
 }
 })
 
-export const { createAnecdote, addVote, appendAnecdote} = anecdoteSlice.actions
+export const { createAnecdote, addVote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
 
