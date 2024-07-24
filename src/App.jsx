70b8295161c35cf7b92c3bfbfee96ec07a7ff11d
Juha-Pekka Heino <juhapekka.heino@brightstraining.com>
2024-07-24 14:09:06 +0300

    tehtävä 6.20

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import axios from 'axios'
+import { getAnecdotes } from './request'
 
 const App = () => {
 
@@ -7,13 +10,21 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+ 
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isSuccess === false) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
 
   return (
     <div>
