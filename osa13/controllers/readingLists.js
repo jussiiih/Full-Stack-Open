67cfce285f2cae6c6705c42d1f8005c48d829ffb
= <=>
2024-08-01 15:54:36 +0300

    tehtävä 13.22

--- osa13/controllers/readingLists.js
+++ osa13/controllers/readingLists.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 const { ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/api/readinglists', async (req, res) => {
     const newReading = await ReadingList.create(req.body)
     res.json(newReading)
+})
+
+router.post('/api/readinglists/:id', tokenExtractor, async (req, res) => {
+    const readingToBeUpdated = await ReadingList.findByPk(req.params.id)
+    readingToBeUpdated.read = req.body.read
+    readingToBeUpdated.save()
+    res.json(readingToBeUpdated)
 })
 
 module.exports = router
--- osa13/util/middleware.js
+++ osa13/util/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
+
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
@@ -25,6 +28,25 @@ const errorHandler = (error, req, res, next) => {
         error: 'Internal Server Error',
         details: error.message || 'An unexpected error occurred'
     })
-};
+}
 
-module.exports = { errorHandler };
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        const token = authorization.substring(7)
+        console.log(`Token received: ${token}`)
+        try {
+            req.decodedToken = jwt.verify(token, SECRET)
+            console.log(`Decoded token: ${JSON.stringify(req.decodedToken)}`)
+        } catch (error) {
+            console.error(`Token verification error: ${error.message}`)
+            return res.status(401).json({ error: 'token invalid' })
+        }
+    } else {
+        return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+}
+
+
+module.exports = { errorHandler, tokenExtractor }
