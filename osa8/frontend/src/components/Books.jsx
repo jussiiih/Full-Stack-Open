52d006b0f946ea964db7b2fa2a1b9daa28b9f33b
= <=>
2024-09-12 09:38:33 +0300

    tehtävä 8.19

--- osa8/frontend/src/components/Books.jsx
+++ osa8/frontend/src/components/Books.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react"
+
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
+  const removeDuplicates = array => array.filter((item, index) => array.indexOf(item) === index)
+
   const books = props.books || []
+  let genreList = books.map(book => book.genres)
+  genreList = genreList.flat()
+  genreList = removeDuplicates(genreList)
+
+
+  const [filter, setFilter] = useState(null)
+  const [allGenres, setAllGenres] = useState(true)
+
+  const genreButtons = genreList.map(genre => <button key={genre} onClick={() => {setFilter(genre); setAllGenres(false);}}>{genre}</button>)
+
+  const genresVisible = allGenres
+    ?  books.map((a) => (
+      <tr key={a.title}>
+        <td>{a.title}</td>
+        <td>{a.author.name}</td>
+        <td>{a.published}</td>
+      </tr>))
+    : books.filter(b => b.genres.includes(filter))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>))
+
 
   return (
     <div>
@@ -16,15 +45,10 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {genresVisible}
         </tbody>
       </table>
+      {genreButtons}<button onClick={() => setAllGenres(true)}>all genres</button>
     </div>
   )
 }
