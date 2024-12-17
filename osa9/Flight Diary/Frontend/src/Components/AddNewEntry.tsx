9f98ec747322607418d892fc7e2ca2a73e8b8c38
= <=>
2024-12-17 16:06:30 +0200

    tehtävä 9.20

--- osa9/Flight Diary/Frontend/src/Components/AddNewEntry.tsx
+++ osa9/Flight Diary/Frontend/src/Components/AddNewEntry.tsx
@@ -25,13 +25,32 @@ const AddNewEntry = (
     return (
         <form onSubmit={addEntry}>
             <div>
-                Date <input value={newDate} onChange={(event) => setNewDate(event.target.value)}/>
+                Date <input type="date" value={newDate} onChange={(event) => setNewDate(event.target.value)}/>
             </div>
             <div>
-                Visibility <input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)}/>
+                Visibility
+                <label htmlFor="great"> Great</label>
+                <input type="radio" id="great" value="great" checked={newVisibility === "great"} onChange={(event) => setNewVisibility(event.target.value)}/>
+                <label htmlFor="good">Good</label>
+                <input type="radio" id="good" value="good" checked={newVisibility === "good"} onChange={(event) => setNewVisibility(event.target.value)}/>
+                <label htmlFor="ok">OK</label>
+                <input type="radio" id="ok" value="ok" checked={newVisibility === "ok"} onChange={(event) => setNewVisibility(event.target.value)}/>
+                <label htmlFor="poor">Poor</label>
+                <input type="radio" id="poor" value="poor" checked={newVisibility === "poor"} onChange={(event) => setNewVisibility(event.target.value)}/>
             </div>
+
             <div>
-                Weather <input value={newWeather} onChange={(event) => setNewWeather(event.target.value)}/>
+                Weather
+                <label htmlFor="sunny"> Sunny</label>
+                <input type="radio" id="sunny" value="sunny" checked={newWeather === "sunny"} onChange={(event) => setNewWeather(event.target.value)}/>
+                <label htmlFor="rainy">Rainy</label>
+                <input type="radio" id="rainy" value="rainy" checked={newWeather === "rainy"} onChange={(event) => setNewWeather(event.target.value)}/>
+                <label htmlFor="cloudy">Cloudy</label>
+                <input type="radio" id="cloudy" value="cloudy" checked={newWeather === "cloudy"} onChange={(event) => setNewWeather(event.target.value)}/>
+                <label htmlFor="stormy">Stormy</label>
+                <input type="radio" id="stormy" value="stormy" checked={newWeather === "stormy"} onChange={(event) => setNewWeather(event.target.value)}/>
+                <label htmlFor="windy">Windy</label>
+                <input type="radio" id="windy" value="windy" checked={newWeather === "windy"} onChange={(event) => setNewWeather(event.target.value)}/>
             </div>
             <div>
                 Comment <input value={newComment} onChange={(event) => setNewComment(event.target.value)}/>
